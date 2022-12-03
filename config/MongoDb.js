b01e1c2164a62a8459665de0b59ec3b511f06368
FragoIt <sanfrago1001@gmail.com>
2022-12-02 20:47:32 -0500

    config again

--- config/MongoDb.js
+++ config/MongoDb.js
@@ -2,14 +2,10 @@ import mongoose from "mongoose";
 
 const connectDatabase = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://tech_admin:7CLgbeJuCR24swvj@dbclustertechlogistics.cc6ja5s.mongodb.net/?retryWrites=true&w=majority', {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
-      options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
-      }
-      
+
     });
 
     console.log(`MongoDB Connected`);
